b4d20e87520f01ba42922bf187744ebf29857d9e
StudyCow <dsit.kr@gmail.com>
2025-06-25 10:46:59 +0900

    first commit

---
+++ src/test/java/com/infrax/mhsafety/MhSafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infrax.mhsafety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhSafetyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
